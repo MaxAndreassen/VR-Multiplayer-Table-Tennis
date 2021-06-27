ee0a1a8dd892a055440ad39b8704ba8319b474fd
Max Andreassen <andreassenmax@gmail.com>
2021-06-27 21:01:52 +0100

    Replaced Unity Physics with Custom Table Tennis Ball Logic
    Whacky physics replaced with working custom logic - very smooth in game now :)

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
@@ -15,28 +13,5 @@ public class Ball : MonoBehaviour
     private void FixedUpdate()
     {
         GetComponent<Rigidbody>().velocity = Vector3.ClampMagnitude(GetComponent<Rigidbody>().velocity, maxSpeed);
-    }
-
-    void OnCollisionEnter (Collision other)
-    {
-        if (other.collider.CompareTag("bat"))
-        {
-            var speed = Mathf.Max(GetComponent<Rigidbody>().velocity.magnitude, minVelocity);
-            var direction = Vector3.Reflect(GetComponent<Rigidbody>().velocity.normalized, other.contacts[0].normal);
-
-            var newVelocity = direction * Mathf.Min(Mathf.Max(speed, minVelocity), maxVelocity);
-
-            GetComponent<Rigidbody>().velocity = new Vector3(
-                newVelocity.x,
-                Mathf.Max(newVelocity.y, GetComponent<Rigidbody>().velocity.y),
-                newVelocity.z
-            );
-
-            return;
-        }
-
-        Debug.Log(other.collider.name);
-        
-        GetComponent<Rigidbody>().AddForce(Vector3.up * strength);
     }
 }
--- Assets/Scripts/Bat.cs
+++ Assets/Scripts/Bat.cs
@@ -1,11 +1,55 @@
+using System;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class Bat : MonoBehaviour
 {
     public float minVelocity = 1;
     public float maxVelocity = 20;
+    
+    public float hitThreshold = 0.5f;
+    public float timeThresholdSeconds = 1f;
 
-    void OnCollisionEnter (Collision other)
+    public GameObject ball;
+
+    public long lastHitInSeconds;
+
+    public Transform hitPoint1;
+    public Transform hitPoint2;
+    public Transform centerPoint;
+
+    public float power = 5f;
+    
+    DateTime? lastHitAt;
+
+    private void Update()
     {
+        if (Vector3.Distance(centerPoint.position, ball.transform.position) < hitThreshold)
+        {
+            if (lastHitAt == null || (DateTime.Now - lastHitAt.Value).TotalSeconds > timeThresholdSeconds)
+            {
+                lastHitAt = DateTime.Now;
+                lastHitInSeconds = lastHitAt.Value.Ticks / 1000;
+
+                var distanceFromHitPoint1 = Vector3.Distance(hitPoint1.position, ball.transform.position);
+                var distanceFromHitPoint2 = Vector3.Distance(hitPoint2.position, ball.transform.position);
+
+                if (distanceFromHitPoint1 < distanceFromHitPoint2)
+                {
+                    ball.GetComponent<Rigidbody>().velocity = hitPoint1.forward * power;
+                }
+                else
+                {
+                    ball.GetComponent<Rigidbody>().velocity = hitPoint2.forward * power;
+                }
+            }
+        }
+
+        Debug.DrawRay(centerPoint.position, ball.transform.position - transform.position, Color.red);
+
+        Debug.DrawRay(centerPoint.position, hitPoint1.forward, Color.blue);
+        
+        Debug.DrawRay(centerPoint.position, hitPoint2.forward, Color.green);
     }
 }
